0cfc68d8452079d71f6e806705618f9e5221e46d
CURTIS\maste <curtisbyrd04@gmail.com>
2026-05-25 20:02:19 -0400

    commiting the screen changes, and rebuilding the project environment

--- database.py
+++ database.py
@@ -8,27 +8,28 @@ Stores the 8 realm puzzles from blue prince
 Key = name (The associate country)
 Element = (Board Layout , Solution) 
 """
+DEFAULT_BOARD_SIZE = 3
 REALM_PUZZLES = {
     "ORINDA ARIES": ([["GREEEN","BLACK","GREEN"],["BLACK","BLACK","BLACK"],["GREEN","YELLOW","GREEN"]],
-                     ["BLACK"] * 4),
+                     ["BLACK"] * 4, DEFAULT_BOARD_SIZE),
     "FENN ARIES": ([["GREY","GREEN","GREY"],["ORANGE","RED","ORANGE"],["WHITE","GREEN","BLACK"]],
-                   ["RED"] * 4),
+                   ["RED"] * 4, DEFAULT_BOARD_SIZE),
     "ARCH ARIES": ([["BLACK","YELLOW","GREY"],["YELLOW","GREEN","YELLOW"],["GREY","YELLOW","BLACK"]],
-                   ["YELLOW"] * 4),
+                   ["YELLOW"] * 4, DEFAULT_BOARD_SIZE),
     "ERAJA ARIES": ([["YELLOW","PURPLE","YELLOW"],["GREEN","RED","BLACK"],["PURPLE","PURPLE","PURPLE"]],
-                    ["PURPLE"] * 4),
+                    ["PURPLE"] * 4, DEFAULT_BOARD_SIZE),
     "Corarica": ([["ORANGE","BLACK","ORANGE"],["ORANGE","ORANGE","ORANGE"],["PURPLE","GREEN","PRURPLE"]],
-                 ["ORANGE"] * 4),
+                 ["ORANGE"] * 4,DEFAULT_BOARD_SIZE),
     "Mora Jai": ([["YELLOW","YELLOW","YELLOW"],["WHITE","PINK","WHITE"],["GREY","GREY","WHITE"]],
-                 ["WHITE"] * 4),
+                 ["WHITE"] * 4, DEFAULT_BOARD_SIZE),
     "Verra": ([["PINK","PINK","GREY"],["GREY","GREY","GREY"],["ORANGE","ORANGE","ORANGE"]],
-              ["PINK"] * 4),
+              ["PINK"] * 4,DEFAULT_BOARD_SIZE),
     "NUANCE": ([["GREEN","GREY","GREEN"],["GREY","ORANGE","ORANGE"],["GREY","BLACK","PURPLE"]],
-               ["GREEN"] * 4),
+               ["GREEN"] * 4, DEFAULT_BOARD_SIZE),
 }
 
 """
-Funciton to print the board database for testing and verification purposes
+Function to print the board database for testing and verification purposes
 """
 def print_boards():
     pass
@@ -45,6 +46,7 @@ def init_database():
             board_name TEXT NOT NULL,
             board_layout TEXT NOT NULL,
             board_solution TEXT NOT NULL,
+            board_size INTEGER NOT NULL,
             )
         '''
         )
@@ -54,12 +56,12 @@ def init_database():
         count = cursor.fetchone()[0]
 
         initial_boards =  [
-            (name, json.dumps(board), json.dumps(solution))
-            for name, board, solution in REALM_PUZZLES.items()
+            (name, json.dumps(board), json.dumps(solution), size)
+            for name, (board, solution, size) in REALM_PUZZLES.items()
         ]
 
         cursor.executemany(
-            """INSERT INTO boards (board_name, board_layout, board_solution) VALUES (?, ?, ?)""",
+            """INSERT INTO boards (board_name, board_layout, board_solution, board_size) VALUES (?, ?, ?)""",
             initial_boards,
         )
         conn.commit()
